a8059a2b79910b357d1c2e984832eadb73853ce7
alexwjj <798595965@qq.com>
2020-06-12 16:45:02 +0800

    关心桥

--- src/pages/layout/index.vue
+++ src/pages/layout/index.vue
@@ -1,30 +1,58 @@
 <template>
   <div class="layout">
-    <div class="layout-header">
-      <div class="layout-logo">奥吉朗</div>
-      <div>
-        <span>admin</span>
-        <span @click="logout" class="logout-btn">退出</span>
-      </div>
-    </div>
-    <div class="layout-main">
-      <div class="layout-side">
-        <div
-          v-for="(item, index) in menuList"
-          :key="index"
-          @click="$router.push({ path: item.path })"
-          class="layout-side-item"
-        >
-          <span :class="activePath === item.path ? 'layout-side-active': ''">{{ item.name }}</span>
+    <div style="height: 60px;background-color:white; position:relative;">
+      <div class="layout-header">
+        <div class="header-logo">关心桥教育</div>
+        <div style="display:flex; align-items:center">
+          <el-menu
+            :default-active="activeIndex"
+            class="header-menu"
+            text-color="#5d5e5d"
+            active-text-color="#e72934"
+            mode="horizontal"
+            @select="handleSelect"
+          >
+            <el-menu-item index="1">精品课程</el-menu-item>
+            <el-menu-item index="2">云课堂</el-menu-item>
+            <el-menu-item index="3">素养</el-menu-item>
+            <el-menu-item index="4">微课中心</el-menu-item>
+            <el-menu-item index="5">下载客户端</el-menu-item>
+          </el-menu>
         </div>
       </div>
-      <div class="layout-content">
-        <router-view></router-view>
-      </div>
+              <div class="header-right">
+          <el-autocomplete
+          popper-class="my-autocomplete"
+          v-model="state"
+          size="mini"
+          multiple
+          :fetch-suggestions="querySearch"
+          placeholder="请输入内容"
+          @select="handleSelect2"
+          clearable
+        >
+          <i
+            class="el-icon-search el-input__icon"
+            slot="suffix"
+            @click="handleIconClick"
+          >
+          </i>
+          <template slot-scope="{ item }">
+            <div class="name">{{ item.value }}</div>
+          </template>
+        </el-autocomplete>
+        <div>
+          <el-button class='btn-login' size="mini">登录</el-button>
+          <el-button type="danger" size="mini">注册</el-button>
+        </div>
+        </div>
     </div>
-    <div class="layout-footer">
-      杭州奥吉朗物联网有限公司
+    <div class="layout-main">
+      <router-view></router-view>
     </div>
+    <!-- <div class="layout-footer">
+      footer
+    </div> -->
   </div>
 </template>
 
@@ -34,91 +62,120 @@ export default {
   components: {},
   data() {
     return {
+      options: [{
+          value: '选项1',
+          label: '黄金糕'
+        }, {
+          value: '选项2',
+          label: '双皮奶'
+        }, {
+          value: '选项3',
+          label: '蚵仔煎'
+        }, {
+          value: '选项4',
+          label: '龙须面'
+        }, {
+          value: '选项5',
+          label: '北京烤鸭'
+        }],
+        value1: [],
+      activeIndex: "3",
       menuList: [],
-      userInfo: '',
+      userInfo: "",
+      restaurants: [],
+        state: ''
     };
   },
   watch: {},
   computed: {
-      activePath() {
-          return this.$route.path
-      }
+    activePath() {
+      return this.$route.path;
+    },
   },
   mounted() {
-    console.log(this.$route, "this.$route")
-    this.handleSidebar();
+    this.restaurants = this.loadAll();
   },
   methods: {
-      logout() {
-          sessionStorage.clear();
-          this.$router.push('/login');
-      },
-    handleSidebar() {
-      const routerList = this.$router.options.routes;
-      routerList.forEach((v) => {
-        if (v.name === "layout") {
-          this.menuList.push(...v.children);
-        }
-      });
-      this.$router.push({path: this.menuList[0].path})
-      //   console.log(this.menuList, 'this.menuList');
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
     },
+    querySearch(queryString, cb) {
+        var restaurants = this.restaurants;
+        var results = queryString ? restaurants.filter(this.createFilter(queryString)) : restaurants;
+        // 调用 callback 返回建议列表的数据
+        cb(results);
+      },
+      createFilter(queryString) {
+        return (restaurant) => {
+          return (restaurant.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0);
+        };
+      },
+      loadAll() {
+        return [
+          { "value": "语文", "address": "微医" },
+          { "value": "数学", "address": "微医" },
+          { "value": "英语", "address": "微医" },
+          { "value": "前端", "address": "微医" }
+        ]
+      },
+      handleSelect2(item) {
+        console.log(item);
+      },
+      handleIconClick(ev) {
+        console.log(ev);
+      }
   },
 };
 </script>
 <style lang="less" scoped>
 .layout {
-    .logout-btn{
-        display:inline-block;
-        width: 50px;
-        height: 30px;
-        border: 1px solid #3e7fee;
-        border-radius: 4px;
-        font-size: 16px;
-        line-height:30px;
-        text-align: center;
-        margin-left: 10px;
-    }
+  height: 60px;
   .layout-header {
     display: flex;
-    color: #3e7fee;
-    font-size: 20px;
-    justify-content: space-between;
-    align-items: center;
-    height: 5vh;
-    padding: 0px 30px;
-    border-bottom: 1px solid #ccc;
+    width: 70vw;
+    color: #5d5e5d;
+    background-color: white;
+    margin: 0 auto;
+    .header-logo {
+      width: 10vw;
+      height: 60px;
+      line-height: 60px;
+      font-size: 20px;
+    }
+    .header-menu {
+      display: flex;
+      width: 30vw;
+    }
+    
   }
+  .header-right{
+      position: absolute;
+      top: 15px;
+      right: 40px;
+      display: flex;
+      align-items: center;
+      width: 20vw;
+      justify-content: space-between;
+      margin-left: 50px
+    }
+    .btn-login{
+      color: #e72934;
+      border-color: #e72934;
+    }
   .layout-main {
-     display: flex;
-    .layout-side {
-      width: 200px;
-      height: 92vh;
-      background-color: #3e7fee;
-      .layout-side-item {
-        height: 5vh;
-        line-height: 5vh;
-        text-align: center;
-        color: white;
-        cursor: pointer;
-      }
-      .layout-side-active{
-          color: #ffbd62;
-      }
-    }
-    .layout-content {
-      padding: 20px;
-      flex:1;
-    //   background-color: blue;
-    }
+    width: 70vw;
+    margin: 0 auto;
   }
-
-  .layout-footer {
-    height: 3vh;
-    line-height: 3vh;
-    color: white;
-    text-align: center;
-    background-color: black;
+  .el-menu.el-menu--horizontal {
+    border: none;
+  }
+  .el-menu--horizontal > .el-menu-item {
+    height: 40px;
+    margin-right: 20px;
+    line-height: 40px;
+  }
+  .el-button--mini{
+    border-radius: 15px;
   }
 }
 </style>
